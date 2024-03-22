3e49489239807761ef0c94cd1adbe9188db17aa5
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-03-22 17:36:33 +0100

    added second-order Markov approximation

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,34 +1,32 @@
 import random
 
-def build_markov_chain(text):
+def build_markov_chain(text, order=1):
     words = text.split()
     markov_chain = {}
-    for i in range(len(words) - 1):
-        current_word = words[i]
-        next_word = words[i + 1]
-        if current_word in markov_chain:
-            markov_chain[current_word].append(next_word)
+    for i in range(len(words) - order):
+        current_words = tuple(words[i:i+order])  # Tworzenie krotki z określoną liczbą słów
+        next_word = words[i + order]
+        if current_words in markov_chain:
+            markov_chain[current_words].append(next_word)
         else:
-            markov_chain[current_word] = [next_word]
+            markov_chain[current_words] = [next_word]
     return markov_chain
 
-
-def generate_text(markov_chain, length=1000):
-    current_word = random.choice(list(markov_chain.keys()))
-    generated_text = current_word
-    word_count = 1
+def generate_text(markov_chain, order = 1, length=1000):
+    current_words = random.choice(list(markov_chain.keys()))
+    generated_text = ' '.join(current_words)
+    word_count = order
     for _ in range(length):
-        if current_word in markov_chain:
-            next_word = random.choice(markov_chain[current_word])
+        if current_words in markov_chain:
+            next_word = random.choice(markov_chain[current_words])
             generated_text += ' ' + next_word
-            current_word = next_word
+            current_words = current_words[1:] + (next_word,)  # Aktualizacja krotki słów
             word_count += 1
             if word_count % 10 == 0:
                 generated_text += '\n'
         else:
             break
     return generated_text
-
 
 def read_random_sample(file_name, sample_size=1000):
     with open(file_name, 'r', encoding='utf-8') as file:
@@ -38,9 +36,15 @@ def read_random_sample(file_name, sample_size=1000):
         text = text[start_index:start_index + sample_size]
     return text
 
-
 sample_text = read_random_sample("norm_hamlet.txt", 1200)
-markov_chain = build_markov_chain(sample_text)
-generated_text = generate_text(markov_chain)
+markov_chain_first_order = build_markov_chain(sample_text)
+text_first_order = generate_text(markov_chain_first_order)
 
-print(generated_text)
+markov_chain_second_order = build_markov_chain(sample_text, 2)
+text_second_order = generate_text(markov_chain_second_order, 2)
+print("First order \n")
+print(text_first_order + "\n")
+print("Second order \n")
+print(text_second_order + "\n")
+
+
